882e6eab5e9714d13a12e109ddee8dd58c1f4905
Unova98 <david_santiago98@hotmail.com>
2020-06-02 15:25:00 -0600

    beta1

--- course/urls.py
+++ course/urls.py
@@ -3,6 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='core'),
-    path('cursos', views.courses, name='cursos'),
+    path('', views.cursoscore, name='core'),
+    path('cursos', views.cursos, name='cursos'),
+    path('cursosing', views.cursosing, name='cursosing'),
+    path('cursosdoc', views.cursosdoc, name='cursosdoc'),
+    path('cursoshora', views.cursoshora, name='cursoshora'),
+    path('cursosfecha', views.cursosfecha, name='cursosfecha'),
+    path('cursosaula', views.cursosaula, name='cursosaula'),
+    path('cursosreg', views.cursosreg, name='cursosreg'),
+    path('postreg', views.postreg, name='postreg'),
 ]
--- localpage/urls.py
+++ localpage/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('course.urls')),
     path('cursos/', include('course.urls')),
+    path('register/', include('course.urls')),
 ]
